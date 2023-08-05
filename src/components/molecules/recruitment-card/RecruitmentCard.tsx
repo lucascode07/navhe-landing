f93585048633bcee3a017fba59a31639223da87f
Lucas Barrenechea <lucascode07@gmail.com>
2023-08-05 17:59:15 -0500

    feat: se agrega página de reclutamiento con los 3 tipos de formularios y sus respectivas validaciones

---
+++ src/components/molecules/recruitment-card/RecruitmentCard.tsx
@@ -0,0 +1,29 @@
+import './RecruitmentCard.scss';
+
+interface Props {
+    data: {
+        image: string;
+        title: string;
+        description: string;
+        buttonCopy: string;
+        type: string;
+    }
+    sendType: (type: string) => void
+}
+
+const RecruitmentCard = ({ data, sendType }: Props) => {
+    return (
+        <div className='m-recruitment-card'>
+            <img src={data.image} alt="imagen  de acompañamiento" />
+            <h2>{data.title}</h2>
+            <p>{data.description}</p>
+            <button
+                className='btn-primary'
+                onClick={() => sendType(data.type)}>
+                {data.buttonCopy}
+            </button>
+        </div>
+    )
+}
+
+export default RecruitmentCard
